f63d19fcde1be4a945325575e94eb4d9e79a4e58
dayanaCEI <82168957+dayanaCEI@users.noreply.github.com>
2021-12-21 21:57:30 +0100

    Motor de plantillas ejs

---
+++ ejercicio43-MotorPlantillas Node/app.js
@@ -0,0 +1,36 @@
+const { response } = require("express");
+const express = require("express");
+const server = express();
+
+//especificar  el  motor  de plantillas, html dinamicos
+server.set("view engine", "ejs");
+server.set("views", __dirname + "/views");
+
+//configuracion servidor estatico, js, css
+server.use(express.static(__dirname + "/public"));
+console.log("hola como estas");
+
+server.get("/", (req, resp) => {
+    resp.render("index", { titulo: "pagina principal", subtitulo: "este es un titulo secundario" });
+})
+
+server.get("/servicios", (req, resp) => {
+    resp.render("servicios");
+})
+
+server.get("/producto", (req, resp) => {
+    resp.render("producto", {
+        nombre: "Cono de trafico",
+        precio: 100,
+        img: "cone.jpeg"
+    })
+})
+
+server.use((req, resp) => {
+    resp.status(404).render("404")
+})
+
+const puerto = 3000;
+server.listen(puerto, () => {
+    console.log("estoy escuchando al servidor")
+})
